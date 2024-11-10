613d7c1e82298be3960e5f67649a80bc70c8d8dd
Mike van den Berge <mike.van.den.berge@gmail.com>
2024-11-10 21:16:46 +0100

    Removing text from return JSON

--- functions/facets.ts
+++ functions/facets.ts
@@ -54,7 +54,7 @@ export const handler = async (event: HandlerEvent) => {
         headers: {
             'Content-Type': 'application/json'
           },
-        body: JSON.stringify({"text": body.text, facet}),
+        body: JSON.stringify({facet}),
     }
 }
 
