b384f3de1b7b8fc61e3f392a6c993733281d694b
Mike van den Berge <mike.van.den.berge@gmail.com>
2024-11-11 12:31:48 +0100

    Added content-type

--- functions/status.ts
+++ functions/status.ts
@@ -2,6 +2,9 @@ module.exports.handler = async (event, context) => {
   const message = `{"status": "OK"}`
 
   return {
+    headers: {
+      'Content-Type': 'application/json'
+    },
     statusCode: 200,
     body: message
   }
